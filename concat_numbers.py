2a31066e6d0051ddd8bb667b0d6c77bba21233e2
bitelxux <bitelxux@gmail.com>
2016-10-02 19:34:57 +0100

    hook test

--- concat_numbers.py
+++ concat_numbers.py
@@ -3,7 +3,6 @@ Build a string with the numbers from 0 to 100, "0123456789101112..."
 We may want to use str.join rather than appending a number every time.
 """
 
-
 def concat_numbers():
     """
     Concat with join
