4b0812d0cd383fb53358e3328fb18edb6432ac12
bitelxux <bitelxux@gmail.com>
2016-10-02 22:23:35 +0100

    list name

--- biggest_3_product.py
+++ biggest_3_product.py
@@ -4,12 +4,12 @@ Get the biggest product give by three numbers in a list.
 
 import itertools
 
-LIST = [-1, -30, 3, 4, 5, 6]
+numlist = [-1, -30, 3, 4, 5, 6]
 
 def get_result():
-    LIST.sort()
-    three_max = LIST[-3:]
-    two_min = LIST[:2]
+    numlist.sort()
+    three_max = numlist[-3:]
+    two_min = numlist[:2]
     print three_max, two_min
     three = reduce(lambda x,y: x*y, three_max)
     two = reduce(lambda x,y: x*y, two_min)
@@ -20,7 +20,7 @@ def get_result_comb():
     broute force solution
     """
     maximum = 0
-    combs = itertools.combinations(LIST, 3)
+    combs = itertools.combinations(numlist, 3)
     for comb in combs:
         res = reduce(lambda a, b: a*b, comb)
         maximum = max(maximum, res)
