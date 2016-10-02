338bcb6917ff371d3ee6fe17be3150990bbf5392
bitelxux <bitelxux@gmail.com>
2016-10-02 22:22:01 +0100

    hook test

--- find_orphan.py
+++ find_orphan.py
@@ -2,14 +2,14 @@
 Get the not repeated id in a list with pairs
 """
 
-LIST = [1, 2, 3, 3, 2, 4, 1, 5, 6, 5, 6]
+numlist = [1, 2, 3, 3, 2, 4, 1, 5, 6, 5, 6]
 
 
 def get_orphan():
 
     orphan_id = 0
 
-    for item in LIST:
+    for item in numlist:
         orphan_id ^= item
 
     return orphan_id
