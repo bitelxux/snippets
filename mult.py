d1ace3bea38f3af64cdc37e1d97ca7defb70b3f7
bitelxux <bitelxux@gmail.com>
2016-10-05 15:37:35 +0100

    tidyup

--- mult.py
+++ mult.py
@@ -4,17 +4,17 @@ product of every integer except the integer at that index.
 Do not use division !!
 """
 
-m = [1+i%4 for i in xrange(0, 5000)]
+m = [1+i%4 for i in xrange(0, 3)]
 result = []
 
 for i in range(len(m)):
   lside = m[:i]
   rside = m[i+1:]
 
-  left = reduce(lambda x,y: x*y, len(lside) > 1 and lside or lside + [1])
-  right = reduce(lambda x,y: x*y, len(rside) > 1 and rside or rside + [1])
+  left = reduce(lambda x,y: x*y, lside and lside or [1])
+  right = reduce(lambda x,y: x*y, rside and rside or [1])
 
-  #print "%d: %s %s: %d" %(i, lside, rside, left*right)
   result.append(left * right )
 
+#print m
 #print result
