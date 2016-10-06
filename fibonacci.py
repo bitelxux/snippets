7be61846a6a04ec4c3f8454d1b241c113725d8ea
bitelxux <bitelxux@gmail.com>
2016-10-06 15:49:47 +0100

    house keeping

--- fibonacci.py
+++ fibonacci.py
@@ -2,43 +2,42 @@
 fibbonacci
 """
 
-def fib(n):
 
+def fib(n):
     if n == 0:
-       return []
+        return []
     elif n == 1:
-       return [0]
+        return [0]
 
     a, b = 0, 1
     fib_list = [a, b]
 
-    for i in xrange(0, n-2):
+    for _ in xrange(0, n-2):
         fib_list.append(a+b)
         a, b = b, a+b
 
     return fib_list
 
+
 def fib_generator(n):
 
     if n == 0:
-       return
+        return
     elif n == 1:
-       yield 0
-       return
+        yield 0
+        return
 
     a, b = 0, 1
     yield a
     yield b
-    for i in xrange(0, n-2):
-       yield a+b
-       a, b = b, a+b 
+    for _ in xrange(0, n-2):
+        yield a+b
+        a, b = b, a+b
 
 for n in 0, 1, 2, 3, 10:
-  print "n = {} No generator".format(n)
-  result = fib(n)
-  print result
-  print "n = {} With generator".format(n)
-  result =  list(fib_generator(n))
-  print result
-
-
+    print "n = {} No generator".format(n)
+    result = fib(n)
+    print result
+    print "n = {} With generator".format(n)
+    result =  list(fib_generator(n))
+    print result
