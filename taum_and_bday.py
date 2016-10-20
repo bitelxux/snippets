2dea58ba985e1a2db92915dfe78426d58de74994
bitelxux <bitelxux@gmail.com>
2016-10-20 17:34:09 +0100

    better names

--- taum_and_bday.py
+++ taum_and_bday.py
@@ -5,14 +5,14 @@ import sys
 
 t = int(raw_input().strip())
 for a0 in xrange(t):
-    min_price = float('inf')
+    best_price = float('inf')
     b,w = raw_input().strip().split(' ')
     b,w = [long(b),long(w)]
     x,y,z = raw_input().strip().split(' ')
     x,y,z = [long(x),long(y),long(z)]
     
-    cb = min(x, y + z)
-    cw = min(y, x + z)
+    cost_b = min(x, y + z)
+    cost_w = min(y, x + z)
     
-    min_price = min(min_price, b * cb + w * cw) 
-    print min_price
+    best_price = min(best_price, b * cost_b + w * cost_w) 
+    print best_price
